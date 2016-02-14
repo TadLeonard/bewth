e00bba02348dde49dc59c9662d89cb1cceb4e8d9
Tad Leonard <tadfleonard@gmail.com>
2016-02-13 21:43:14 -0600

    adding pkg to setup.py, tweaks to project description

--- setup.py
+++ setup.py
@@ -5,20 +5,9 @@ from setuptools import setup
 version = "v0.0.1-alpha"
 url = "https://github.com/TadLeonard/bewth"
 download = "{}/archive/{}.tar.gz".format(url, version)
-
-# NOTE: pandoc doesn't convert Markdown's in-word-emphasis,
-# so I have to upload a different hand-tuned README to PyPI
-# PyPI might also just be terrible at rendering RST 
-
-#try:
-#    import pypandoc
-#    long_description = pypandoc.convert('README.md', 'rst')
-#except ImportError:
-#    long_description = ("Tools for syncing files with the Toshiba FlashAir "
-#                        "wireless SD card")
-
-long_description= "See {} for documentation".format(url)
-description = "A photo booth"
+long_description= """A photo booth for use with the Toshiba FlashAir
+wireless SD card. See {} for documentation""".format(url)
+description = "A photo booth for use with the Toshiba FlashAir wireless SD card"
 
 classifiers = [
   'Intended Audience :: End Users/Desktop',
@@ -29,6 +18,7 @@ classifiers = [
 
 setup(name="bewth",
       version=version,
+      packages=["bewthware"],
       scripts=["bewth"],
       licence="MIT",
       description=description,
@@ -38,7 +28,7 @@ setup(name="bewth",
       package_data={"": ["README.md"]},
       author="Tad Leonard",
       author_email="tadfleonard@gmail.com",
-      keywords="photo booth photobooth", 
+      keywords="photo booth photobooth toshiba flashair wireless sd card",
       url=url,
       download_url=download,
       zip_safe=True,
